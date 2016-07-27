132c258127745114aea1ffd29634ab6ab4ef47fc
Troy Chryssos <github email address>
2016-07-27 10:34:22 -0400

    Created Anagram class that can compare a given word to an array of words to find anagrams by splitting strings and comparing the sorted arrays.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word=word
+  end
+
+  def match(word_array)
+    anagram_matches=[]
+    word_array.each do |ana_words|
+      anagram_matches<<ana_words if ana_words.split("").sort == @word.split("").sort
+    end
+    anagram_matches
+  end
+end
